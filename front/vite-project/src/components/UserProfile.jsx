94e5f439dd4fec259f6137288de4fa593e959f15
jorge <jorgemartinez.jam@gmail.com>
2024-11-26 21:37:19 -0300

    Nuevas validaciones register form

--- front/vite-project/src/components/UserProfile.jsx
+++ front/vite-project/src/components/UserProfile.jsx
@@ -1,23 +1,23 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React from "react";
+import { useSelector } from "react-redux";
 import styles from "../styles/UserProfile.module.css";
 
 const UserProfile = () => {
-    const user = useSelector((state) => state.user.user);
+  const user = useSelector((state) => state.user.user);
 
-    return (
-        <div className={styles.userProfile}>
-            {user ? (
-                <div>
-                    <h2>{user.name}'s Profile</h2>
-                    <p>Email: {user.email}</p>
-                    {/* Aquí podrías añadir más detalles del perfil */}
-                </div>
-            ) : (
-                <p>Please log in to see your profile details.</p>
-            )}
+  return (
+    <div className={styles.userProfile}>
+      {user ? (
+        <div>
+          <h2>{user.name}'s Profile</h2>
+          <p>Email: {user.email}</p>
+          {/* Aquí podrías añadir más detalles del perfil */}
         </div>
-    );
+      ) : (
+        <p>Por favor inicia sesión para ver los detalles del perfil.</p>
+      )}
+    </div>
+  );
 };
 
 export default UserProfile;
--- front/vite-project/src/helpers/validateRegisterForm.js
+++ front/vite-project/src/helpers/validateRegisterForm.js
@@ -1,45 +1,62 @@
 const validateRegisterForm = (values) => {
-    const errors = {};
+  const errors = {};
 
-    if (!values.username) {
-        errors.username = 'Username es requerido';
-    } else if (values.username.length > 30) {
-        errors.username = 'Username no puede tener más de 30 caracteres';
+  if (!values.username) {
+    errors.username = "Username es requerido";
+  } else if (values.username.length > 30) {
+    errors.username = "Username no puede tener más de 30 caracteres";
+  }
+
+  if (!values.password) {
+    errors.password = "Password es requerido";
+  } else if (values.password.length < 6) {
+    errors.password = "Password debe tener al menos 6 caracteres";
+  }
+
+  if (!values.name) {
+    errors.name = "Nombre y Apellido es requerido";
+  } else if (/[^a-zA-ZÀ-ÿ\s]/.test(values.name)) {
+    // Permite letras con acentos y espacios
+    errors.name =
+      "Nombre y Apellido no puede contener caracteres especiales como !*+_-¡";
+  } else if (values.name.length > 100) {
+    errors.name = "Nombre y Apellido no puede tener más de 100 caracteres";
+  }
+
+  if (!values.email) {
+    errors.email = "Email es requerido";
+  } else if (!/\S+@\S+\.\S+/.test(values.email)) {
+    errors.email = "Dirección de email inválida";
+  }
+
+  if (!values.birthdate) {
+    errors.birthdate = "Fecha de Nacimiento es requerida";
+  } else {
+    const birthDate = new Date(values.birthdate);
+    const today = new Date();
+    const age = today.getFullYear() - birthDate.getFullYear();
+    const isFutureDate = birthDate > today;
+
+    if (isFutureDate) {
+      errors.birthdate = "La fecha de nacimiento no puede estar en el futuro";
+    } else if (
+      age < 18 ||
+      (age === 18 &&
+        today < new Date(birthDate.setFullYear(today.getFullYear())))
+    ) {
+      errors.birthdate = "Debe ser mayor de 18 años";
     }
+  }
 
-    if (!values.password) {
-        errors.password = 'Password es requerido';
-    } else if (values.password.length < 6) {
-        errors.password = 'Password debe tener al menos 6 caracteres';
-    }
+  if (!values.nDni) {
+    errors.nDni = "El número de DNI es requerido";
+  } else if (isNaN(values.nDni)) {
+    errors.nDni = "El DNI debe ser un número";
+  } else if (values.nDni.length > 10) {
+    errors.nDni = "El DNI no puede tener más de 10 caracteres";
+  }
 
-    if (!values.name) {
-        errors.name = 'Nombre y Apellido es requerido';
-    } else if (/[^a-zA-ZÀ-ÿ\s]/.test(values.name)) {  // Permite letras con acentos y espacios
-        errors.name = 'Nombre y Apellido no puede contener caracteres especiales como !*+_-¡';
-    } else if (values.name.length > 100) {
-        errors.name = 'Nombre y Apellido no puede tener más de 100 caracteres';
-    }    
-
-    if (!values.email) {
-        errors.email = 'Email es requerido';
-    } else if (!/\S+@\S+\.\S+/.test(values.email)) {
-        errors.email = 'Dirección de email invalida';
-    }
-
-    if (!values.birthdate) {
-        errors.birthdate = 'Fecha de Nacimiento es requerido';
-    }
-
-    if (!values.nDni) {
-        errors.nDni = 'El número de DNI es requerido';
-    } else if (isNaN(values.nDni)) {
-        errors.nDni = 'El DNI debe ser un número';
-    } else if (values.nDni.length > 20) {
-        errors.nDni = 'El DNI no puede tener más de 20 caracteres';
-    }
-
-    return errors;
+  return errors;
 };
 
 export default validateRegisterForm;
